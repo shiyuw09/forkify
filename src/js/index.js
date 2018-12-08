d63314c960e260a084c53b5f454ca56f193a1dda
wangsy <wangsy@fenbi.com>
2018-12-08 22:35:30 +0800

    Building the Recipe Model - Part 2

--- src/js/index.js
+++ src/js/index.js
@@ -29,7 +29,7 @@ const controlSearch = async () => {
             // 5. Render results on UI
             clearLoader();
             searchView.renderResults(state.search.result);
-            
+
         } catch (error) {
             alert('Something wrong with the search...');
             clearLoader();
@@ -66,8 +66,9 @@ const controlRecipe = async () => {
         state.recipe = new Recipe(id);
 
         try{
-            // Get recipe data
+            // Get recipe data and parse ingredients
             await state.recipe.getRecipe();
+            state.recipe.parseIngredients();
     
             // Calculate servings and time
             state.recipe.calcTime();
