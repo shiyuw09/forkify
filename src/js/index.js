ada197e8c2fb530ed88b35538a8df9282c4c5957
wangsy <wangsy@fenbi.com>
2018-12-08 19:34:28 +0800

    Building the Recipe Controller

--- src/js/index.js
+++ src/js/index.js
@@ -22,12 +22,18 @@ const controlSearch = async () => {
         searchView.clearResults();
         renderLoader(elements.searchRes);
 
-        // 4. Search for recipes
-        await state.search.getResults();
-
-        // Render results on UI
-        clearLoader();
-        searchView.renderResults(state.search.result);
+        try {
+            // 4. Search for recipes
+            await state.search.getResults();
+    
+            // 5. Render results on UI
+            clearLoader();
+            searchView.renderResults(state.search.result);
+            
+        } catch (error) {
+            alert('Something wrong with the search...');
+            clearLoader();
+        }
     }
 }
 
@@ -48,6 +54,31 @@ elements.searchResPages.addEventListener('click', e => {
 /**
  * RECIPE CONTROLLER
  */
-const r = new Recipe(46956);
-r.getRecipe();
-console.log(r);
+const controlRecipe = async () => {
+    // Get ID from url
+    const id = window.location.hash.replace('#','');
+    console.log(id);
+
+    if(id) {
+        // Prepare UI for changes
+
+        // Create new recipe object
+        state.recipe = new Recipe(id);
+
+        try{
+            // Get recipe data
+            await state.recipe.getRecipe();
+    
+            // Calculate servings and time
+            state.recipe.calcTime();
+            state.recipe.calcServings();
+    
+            // Render recipe
+            console.log(state.recipe);
+        } catch (error) {
+            alert('Error processing recipe!');
+        }
+    }
+};
+
+['hashchange', 'load'].forEach(event => window.addEventListener(event, controlRecipe));
